7ffe69e111bc67a40a15459461c3930d457f50fb
Jacob Alarcon <jacobalarcon@gmail.com>
2023-05-08 15:17:20 -0500

    complete simple login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AppRoutes from './routes';
 
 const rootElement = document.getElementById('root');
 
@@ -8,9 +8,16 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <AppRoutes />
     </React.StrictMode>
   );
 } else {
   console.error('Failed to find the root element');
 }
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <AppRoutes />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
