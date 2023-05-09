bc01cc84cbc2372b70be843e29c49960f8ab0d6a
Kudrat Singh <kudratsingh@Kudrats-MacBook-Pro.local>
2023-05-09 07:52:06 -0700

    signup and navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import { CSSProperties } from 'react';
+
+const NavBar: React.FC = () => {
+  const navbarStyle: CSSProperties = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#222',
+    padding: '0 20px',
+  };
+
+  const linkStyle: CSSProperties = {
+    color: 'white',
+    textDecoration: 'none',
+    margin: '0 10px',
+  };
+
+  const navLinksStyle: CSSProperties = {
+    display: 'flex',
+  };
+
+  return (
+    <nav style={navbarStyle}>
+      <a href="/" style={linkStyle}>
+        Home
+      </a>
+      <div style={navLinksStyle}>
+        <a href="/sign-up" style={linkStyle}>
+          Sign Up
+        </a>
+        <a href="/sign-in" style={linkStyle}>
+          Sign In
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
